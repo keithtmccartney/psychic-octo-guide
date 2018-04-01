d5c88240be4a07e8480fb9dfd08dc2989582979e
Keith <keithtmccartney@hotmail.com>
2018-04-01 17:32:39 +0100

    Adding 'Block' and 'StringUtil' Tests.

---
+++ noobchain/noobchain.Tests/BlockTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using noobchain.Library;
+using noobchain.Library.Helpers;
+
+namespace noobchain.Tests
+{
+    [TestClass]
+    public class BlockTest
+    {
+        String _data = "Hi im the first block";
+        String _previousHash = "0";
+
+        [TestMethod]
+        public void BlockTestMethod()
+        {
+            Block block = new Block(_data, _previousHash);
+        }
+
+        [TestMethod]
+        public void BlockCalculateHashTestMethod()
+        {
+            Block block = new Block(_data, _previousHash);
+
+            block.calculateHash();
+        }
+    }
+}
---
+++ noobchain/noobchain.Tests/StringUtilTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using noobchain.Library;
+using noobchain.Library.Helpers;
+
+namespace noobchain.Tests
+{
+    [TestClass]
+    public class StringUtilTest
+    {
+        String _data = "Hi im the first block";
+        String _previousHash = "0";
+
+        [TestMethod]
+        public void StringUtilTestMethod()
+        {
+            StringUtil stringUtil = new StringUtil();
+        }
+
+        [TestMethod]
+        public void StringUtilApplySha256TestMethod()
+        {
+            StringUtil.applySha256(_data);
+        }
+    }
+}
